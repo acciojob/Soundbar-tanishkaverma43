77b58102ee19a581846d8be1b713472164fea27c
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-05 18:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-//your JS code here. If required.
+let audio;
+
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', function() {
-        // code to play sound
+        // stop any currently playing audio
+        if (audio) {
+            audio.pause();
+        }
+
+        // create a new Audio object and play it
+        audio = new Audio(`./sounds/${this.innerText}.mp3`);
+        audio.play();
     });
 });
 
 document.querySelector('.stop').addEventListener('click', function() {
-    // code to stop sound
+    // stop the audio if it's playing
+    if (audio) {
+        audio.pause();
+    }
 });
