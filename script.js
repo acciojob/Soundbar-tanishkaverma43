8ac51f3b12e578b4642f45487c68e55847e36447
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-05 18:47:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        // code to play sound
+    });
+});
+
+document.querySelector('.stop').addEventListener('click', function() {
+    // code to stop sound
+});
